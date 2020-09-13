0d585f786333f5d8d8b1da49497cef499bf54859
Victoria Olivia <victoria@iMac-de-Victoria.local>
2020-09-12 21:00:51 -0300

    add value_objects with resource definitions and handler function

--- src/v1/value_objects.go
+++ src/v1/value_objects.go
@@ -1,6 +1,10 @@
 package v1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
 
 // YdataSpec defines all attributes in spec
 type YdataSpec struct {
@@ -13,4 +17,69 @@ type Ydata struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec YdataSpec `json:"spec"`
+}
+
+type YdataList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Ydata `json:"items"`
+}
+
+// DeepCopyInto copies all properties of this object into another object of the
+// same type that is provided as a pointer.
+func (in *Ydata) DeepCopyInto(out *Ydata) {
+	out.TypeMeta = in.TypeMeta
+	out.ObjectMeta = in.ObjectMeta
+	out.Spec = YdataSpec{
+		Replicas: in.Spec.Replicas,
+	}
+}
+
+// DeepCopyObject returns a generically typed copy of an object
+func (in *Ydata) DeepCopyObject() runtime.Object {
+	out := Ydata{}
+	in.DeepCopyInto(&out)
+
+	return &out
+}
+
+// DeepCopyObject returns a generically typed copy of an object
+func (in *YdataList) DeepCopyObject() runtime.Object {
+	out := YdataList{}
+	out.TypeMeta = in.TypeMeta
+	out.ListMeta = in.ListMeta
+
+	if in.Items != nil {
+		out.Items = make([]Ydata, len(in.Items))
+		for i := range in.Items {
+			in.Items[i].DeepCopyInto(&out.Items[i])
+		}
+	}
+
+	return &out
+}
+
+const (
+	// GroupName defines the name of a group
+	GroupName = "ydata.ai"
+	// GroupVersion defines the name of the version
+	GroupVersion = "v1"
+)
+
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: GroupVersion}
+
+var (
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Ydata{},
+		&YdataList{},
+	)
+
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
 }
